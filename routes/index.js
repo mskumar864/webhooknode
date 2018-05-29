26f860deeff671cf22aeb5680cbf120ecc3b5686
Medasani <smedasani@paypal.com>
2018-05-29 16:15:57 +0530

    new changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var count=0;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', count : count});
+
 });
 
+
+router.post('/register',function (req,res,next) {
+
+   var eventName=req.body.eventDetails.eventName;
+
+   count++;
+
+   res.end('Count is '+ count);
+
+
+})
 module.exports = router;
